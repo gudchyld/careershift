62feedf2e14625cef5c1223351366e37eb27b8e5
Valarie Oyieke <oyiekeav@gmail.com>
2023-05-30 16:58:34 +0300

    contact page update

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
 
 const Contacts = () => {
     return ( 
@@ -7,9 +9,36 @@ const Contacts = () => {
           <title>CareerShiftng | Contacts</title>
         </Head>
 
-        <div className="contacts">
-            This is the Contacts Page
-        </div>
+        <section className="contacts">
+          <div className="contact-image">
+              <Image src='/contact.png' layout="fill"/>
+            </div>
+            <div className="contactForm">
+              <h2>CONTACT US</h2>
+              <p>Got any Questions? We are here to help !</p>
+              <form>
+                 <div className="inputBox">
+                   <input type="text" name="" required="required"/>
+                   <span>FULL NAME</span>
+                 </div>
+                 <div className="inputBox">
+                   <input type="email" name="" required="required"/>
+                   <span>EMAIL</span>
+                 </div>
+                 <div className="inputBox">
+                   <input type="text" name="" required="required"/>
+                   <span>PHONE</span>
+                 </div>
+                 <div className="inputBox">
+                   <textarea required="required"></textarea>
+                   <span>MESSAGE</span>
+                 </div>
+                 <div className="inputBox">
+                   <input type="submit" name="" value="SEND MESSAGE"/>
+                 </div>
+              </form>
+            </div>
+        </section>
         </>
      );
 }
