6833e827f41972cc5514542ec0956ef1db8b7df6
Elendu Ifeanyi <goodchild66@gmail.com>
2023-05-31 19:13:35 +0100

    footer changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,12 @@
-import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaPhoneAlt } from 'react-icons/fa';
+
+import {AiOutlineMail} from 'react-icons/ai'
+import {FiPhone} from 'react-icons/fi'
 import Image from 'next/image'
+import Link from 'next/link'
+
+
+
 const Footer = () => {
     return ( 
         <footer className="footer">
@@ -7,10 +14,15 @@ const Footer = () => {
             <h1 className="logo">CAREERSHIFTng</h1>
             
             <div className="footer-icons">
-                <FaTwitterSquare className="icon"/>
-                <FaInstagramSquare className="icon"/>
-                <FaLinkedin className="icon"/>
-                <FaFacebookSquare className="icon"/>
+                <Link href="/"><FaTwitterSquare className="icon"/></Link>
+                <Link href="/"><FaInstagramSquare className="icon"/></Link>
+                <Link href="/"><FaLinkedin className="icon"/></Link>
+                <Link href="/"><FaFacebookSquare className="icon"/></Link>
+            </div>
+            <div className='footer-message'>
+                <div className='m-align'><FiPhone className='icon-message' /><p> 08135326860</p></div>
+                <div className='m-align'><AiOutlineMail className='icon-message' /> <p> careershiftng@gmail.com</p></div>
+                
             </div>
         </footer>
      );
