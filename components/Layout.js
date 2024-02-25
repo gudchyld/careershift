924f925e0f6d22e4f95b6e5ad541b8ca2fee54f4
gudchyld <goodchild66@gmail.com>
2024-02-25 22:17:29 +0100

    adjust careershift tiltle font

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,18 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import {Poppins} from "next/font/google";
+
+const poppins_init = Poppins({
+   subsets:["latin"],   
+   weight:["100","300","500","700"],
+   variable: "--font-poppins",
+   display: "swap",
+   adjustFontFallback: false,
+ });
 
 const Layout = ({ children }) => {
     return (
-        <div className="content">
+        <div className={`${poppins_init.variable} content `}>
            <Navbar />
            { children }
            <Footer />
